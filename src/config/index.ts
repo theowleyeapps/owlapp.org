dee5ddd423de771d8d020f31bce36069ba08774e
theowleyeapps <theowleyeapp@gmail.com>
2025-08-27 15:47:13 +0530

    Update index.ts

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,8 +1,8 @@
 import type { SiteConfig, SiteContent } from "../types";
 
 export const SITE_CONFIG: SiteConfig = {
-  title: "Alejandro Múnez — Mobile & Web Developer",
-  author: "Alejandro Múnez Cuntez",
+  title: "Ayalin G — Mobile & Web Developer",
+  author: "Ayalin G",
   description:
     "Software Engineer based in San Francisco, USA. I specialize in UI design, web and mobile application development and maintenance.",
   lang: "en",
